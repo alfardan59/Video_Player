722abf21f4a34717ba8e37a7cd22615b3574a3e8
alfardan <alfardanarfin500@gmail.com>
2026-08-13 17:54:31 +0530

    fix: update user registration logic and error handling; correct dotenv path and user model references

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -52,7 +52,7 @@ userSchema.pre('save',async function(req,res,next){
     if(!this.isModified("password")) return next()
 
     this.password = await bcrypt.hash(this.password,10)
-    next()
+    // next()
 })
 
 userSchema.methods.isPasswordCorrect=async function name(password) {
