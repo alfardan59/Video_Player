5d2ab04ebd7378864c7439ab1e102617c5d5de94
alfardan <alfardanarfin500@gmail.com>
2026-08-13 18:12:34 +0530

    fix: improve cover image handling in user registration; ensure proper file path assignment and error handling

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -31,8 +31,13 @@ const registerUser = asyncHandler(async(req,res)=>{
     }
 
     //These two are coming from user route's multer middleware
-    const avatarLocalPath=req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    const avatarLocalPath=req.files?.avatar?.[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required");
